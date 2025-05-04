be112f00492763dd101d8153b9c089a9d3fe20e5
Avinash Limbu <50650789+Avinashlimbu@users.noreply.github.com>
2025-05-05 08:03:29 +0900

    Completed 02_repeat strings
    Bit difficult at first but now i understand for loop
    much better i believe!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,36 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    if (num < 0){
+        return "ERROR";
+    }
+    let addingString = "";
+    for (let i = 1; i <= num; i++) {
+        addingString = addingString + string;
+    }
+    return addingString;
 };
+
+console.log(repeatString("hey", 3))
+
+/* 
+function myString (string, num) {
+    for (; num < 3; num++) {
+        console.log(string)
+    }
+
+}
+
+if num = 3
+repeat the string three time in the same line! 
+if num = 4 
+it should repeart the string 4 times in the same line
+everytime i call the function 
+there should a variable that stores in that string that we want by giging it a number
+
+
+
+
+*/
 
 // Do not edit below this line
 module.exports = repeatString;
