11f574ba73822dda4826e8a3191aeb6dbedc399c
Avinash Limbu <50650789+Avinashlimbu@users.noreply.github.com>
2025-05-05 19:37:29 +0900

    Lesson no 03 completed
    Reversing string is kind of easy, but understanding where the logic is working
    is the most important part!!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,36 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let result = "";
+    if (text.trim().length === 0) {
+        return "";
+    }
+    for (let mySplit of text) {
+        result = text.split("");
+        reverse = result.reverse();
+        joined = reverse.join("");  
+    return joined; // ?? "";
+    }
+    console.log (text);
+    //console.log(reverse);
+    //console.log (joined);
+}
 
-};
 
+console.log(reverseString(""));
+
+
+
+/*
+This is my psuedo code
+A function will return reverse string
+pretty straigh forward
+
+let a = ""
+if that 
+
+let text = "How are you doing today?";
+let mySplit = text.split("");
+
+
+*/
 // Do not edit below this line
 module.exports = reverseString;
