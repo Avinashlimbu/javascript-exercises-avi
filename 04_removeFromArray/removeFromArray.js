fa6f14c241d987be887ab70f9bd6515cd594795f
Avinash Limbu <50650789+Avinashlimbu@users.noreply.github.com>
2025-05-06 11:54:15 +0900

    Exercise 03 completed
    Understood how to add optional arguements
    Splice method for array, also other method type that helps with JS in general

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...value) {
+    for (const elements of value) {
+    let index = arr.length -1;
+    for (;index >= 0; index--) {
+        if (value.includes(arr[index])) {
+        arr.splice(index, 1);
+    }
+    }
+}
+    return arr;
+}
+
+console.log(removeFromArray([1, 2, 2, 3, 4, 5, 6, "john", "johnny", "john"], 3, 2,"johdded"));
+
 
 // Do not edit below this line
+/*
+So i need to make a function that can take two arguements
+
+
+*/
+
 module.exports = removeFromArray;
