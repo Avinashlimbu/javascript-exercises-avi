ad10f8cc6786e220860d596d6070e7adca1b71d2
Avinash Limbu <50650789+Avinashlimbu@users.noreply.github.com>
2025-05-07 21:12:31 +0900

    temprature conversion completed
    relatively easier than the other tasks
    no loops no conditions
    only simple math with math ciel new to the group. that helps your round the number

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let y = fahrenheit;
+  return Math.ceil((((y - 32) * (5/9))) *10)/10; //added 10  for one decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let x = celsius; 
+  return Math.ceil((((x * (9/5)) +  32) *10 ))/10; //added 10  for one decimal place
 };
+
+
+console.log(convertToCelsius(100));
+console.log(convertToFahrenheit(100));
+
+
 
 // Do not edit below this line
+
+/*
+This is where i am writing my pseudo code
+I need to write two function
+one for celcius and the other for fahrenheit
+
+fahrenheit to celsius is below
+x celcius(x) = ((x * (9/5)) +  32)  
+celsius to fahrenheit is below
+fahrenheit(y) = (y-32) x 5/9
+
+*/
+
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
