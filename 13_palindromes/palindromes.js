8f12d680c354b629de728e7db8154c75fa0579b3
Avinash <50650789+Avinashlimbu@users.noreply.github.com.>
2025-05-30 08:46:26 +0900

    palindrome exercise completed
    learned about replace, which takes everything but symbol
    split reverse join, take all that method in join way
    you can fist split a text into arraw of spellings and then reverse it and join it

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let reverse = word.split("").reverse().join("");
+    console.log(reverse);
+    console.log(word);
+    return word === reverse; 
 };
+
+
+console.log(palindromes("racecar"))
+console.log(palindromes("racecar!@#"))
+console.log(palindromes("A car, a man, a maraca."))
+console.log(palindromes('ZZZZ car, a man, a maracaz.'))
+
 
 // Do not edit below this line
 module.exports = palindromes;
