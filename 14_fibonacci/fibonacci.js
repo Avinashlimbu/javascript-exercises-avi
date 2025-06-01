f901c01f26ebbd30e8fe94c7128ea6338db8e8b4
Avinash <50650789+Avinashlimbu@users.noreply.github.com.>
2025-06-01 09:24:39 +0900

    Fibonacci Exercise Completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n === 0 || n === "0") {
+        return 0;
+    }
+    else if (n === 1){
+        return 1;
+    }
+    else if (n < 0) {
+        return "OOPS"
+    }
+
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 
 };
 
+console.log(fibonacci("0"));
 // Do not edit below this line
 module.exports = fibonacci;
