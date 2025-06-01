78a1374a64e4e887ef2742b6779f39184c8789c0
Avinash <50650789+Avinashlimbu@users.noreply.github.com.>
2025-06-01 09:51:58 +0900

    completed map()

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
 const getTheTitles = function() {
+    const data = [
+        {
+            book: "Book",
+            author: "johse"
+        },
+        {
+            book: "Book2",
+            author: "honee"
+        }
+    ];
+    const books = data.map(function(item) {
+        return item.book;
+    });
 
+    return books;
 };
+
+console.log(getTheTitles());
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
